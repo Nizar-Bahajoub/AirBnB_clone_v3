4c0175f8791c6b96faf359ef1b57e2cc7c65711b
nizar <n.bahajoub@gmail.com>
2024-02-06 13:54:30 -0800

    Task 4

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -8,3 +8,12 @@ from flask import jsonify
 @app_views.route("/status")
 def status():
     return jsonify({"status": "OK"})
+
+@app_views.route("/stats")
+def stats_count():
+    stats = {}
+    class_names = ["Amenity", "City", "Place", "Review", "State", "User"]
+    
+    for class_name in class_names:
+        stats[class_name.lower()] = storage.count(class_name)
+    return jsonify(stats)
